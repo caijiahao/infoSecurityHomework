f3c39db5353daeb37316d7e5de70baced427120e
stevenfen <634606228@qq.com>
2016-09-15 14:36:36 +0800

    2016-9-15 信息安全第一次代码

---
+++ informationSecurityHomework1/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace informationSecurityHomework1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.凯撒加密ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vignere加密ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(52, 112);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 162);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "明文";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(484, 112);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(182, 162);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(575, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "密文";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(809, 25);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.凯撒加密ToolStripMenuItem,
+            this.vignere加密ToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(68, 21);
+            this.toolStripMenuItem1.Text = "加密体制";
+            // 
+            // 凯撒加密ToolStripMenuItem
+            // 
+            this.凯撒加密ToolStripMenuItem.Name = "凯撒加密ToolStripMenuItem";
+            this.凯撒加密ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.凯撒加密ToolStripMenuItem.Text = "凯撒加密";
+            this.凯撒加密ToolStripMenuItem.Click += new System.EventHandler(this.凯撒加密ToolStripMenuItem_Click);
+            // 
+            // vignere加密ToolStripMenuItem
+            // 
+            this.vignere加密ToolStripMenuItem.Name = "vignere加密ToolStripMenuItem";
+            this.vignere加密ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.vignere加密ToolStripMenuItem.Text = "Vignere加密";
+            this.vignere加密ToolStripMenuItem.Click += new System.EventHandler(this.vignere加密ToolStripMenuItem_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(307, 170);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(344, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "密钥";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 356);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 凯撒加密ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vignere加密ToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ informationSecurityHomework1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace informationSecurityHomework1
+{
+    public partial class Form1 : Form
+    {
+        private static int kasaLength = 3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 凯撒加密ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string plainText = this.textBox1.Text;
+            char[] ch=plainText.ToCharArray();
+            string chiperText = "";
+            for (int i = 0; i < ch.Length;i++ )
+            {
+                chiperText += plainTextToChiperText(ch[i],3);
+            }
+            this.textBox2.Text = chiperText;
+
+        }
+
+        private void vignere加密ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string plainText = this.textBox1.Text;
+            char[] ch = plainText.ToCharArray();
+            string secertKey = this.textBox3.Text;
+            int[] secertNum = secertKeyTosecertNum(secertKey);
+            string chiperText = "";
+            int count = 0;
+            for (int i = 0; i < ch.Length; i++)
+            {
+                if (ch[i] != ' ')
+                {
+                    chiperText += plainTextToChiperText(ch[i], caculateMaJu(count, secertNum));
+                    count++;
+                }
+                else
+                {
+                    chiperText += ch[i];
+                }
+            }
+            this.textBox2.Text = chiperText;
+        }
+
+        //将密钥转化函数
+        //@param  string 密钥
+        //@return int[] 
+        private int[] secertKeyTosecertNum(string secertKey)
+        {
+            char[] ch = secertKey.ToCharArray();
+            int[] num = new int[ch.Length];
+            for (int i = 0; i < ch.Length; i++)
+            {
+                if (ch[i] >= 'a' && ch[i] <= 'z')
+                {
+                    num[i] = ch[i] - 'a';
+                }
+                else if (ch[i] >= 'A' && ch[i] <= 'Z')
+                {
+                    num[i] = ch[i] - 'A';
+                }
+
+            }
+            return num;
+        }
+
+
+        //单个明文字符转化为密文字符
+        //@param  char 明文
+        //@return char 密文 
+        private char plainTextToChiperText(char plainChar,int codeDistance)
+        {
+            if (plainChar >= 'a' && plainChar <= 'z')
+            {
+                return Convert.ToChar(((plainChar - 'a' + codeDistance) % 26) + 'a');
+            }
+            else if (plainChar >= 'A' && plainChar <= 'Z')
+            {
+                return Convert.ToChar(((plainChar - 'A' + codeDistance) % 26) + 'A');
+            }
+            else
+            {
+                return plainChar;
+            }
+        }
+
+        //根据字符计算码距
+        //@param  int 明文
+        //@param  int[] 密钥码距
+        //@return int 码距 
+        private int caculateMaJu(int plainCharNum, int[] secertNum)
+        {
+            return secertNum[plainCharNum % secertNum.Length];
+        } 
+    }
+}
